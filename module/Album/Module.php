fb534be3ee22ea32921d12bc19d8126917c85920
gbryk <g.bryk24@gmail.com>
2013-12-18 13:06:00 +0100

    Initializing repo on GitHub.

--- module/Album/Module.php
+++ module/Album/Module.php
@@ -1,7 +1,11 @@
 <?php
 
  namespace Album;
-
+ use Album\Model\AlbumTable;
+ use Album\Model\Album;
+ use Zend\Db\ResultSet\ResultSet;
+ use Zend\Db\TableGateway\TableGateway;
+ 
  class Module
  {
      public function getAutoloaderConfig()
@@ -21,5 +25,23 @@
      public function getConfig()
      {
          return include __DIR__ . '/config/module.config.php';
+     }
+     
+     public function getServiceConfig(){
+         return array(
+             'factories' =>array(
+                 'Album\Model\AlbumTable' => function($sm) {
+                    $tableGateway = $sm->get('AlbumTableGateway');
+                    $table = new AlbumTable($tableGateway);
+                    return $table;
+                 },
+                 'AlbumTableGateway' => function ($sm){
+                     $dbAdapter = $sm->get('Zend\Db\Adapter\Adapter');
+                     $resultSetPrototype = new ResultSet();
+                     $resultSetPrototype->setArrayObjectPrototype(new Album());
+                     return new TableGateway('album', $dbAdapter, null, $resultSetPrototype);
+                 },
+             ),
+         );
      }
  }
--- module/Album/src/Album/Controller/AlbumController.php
+++ module/Album/src/Album/Controller/AlbumController.php
@@ -6,10 +6,15 @@ use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
 
 class AlbumController extends AbstractActionController
-{
-    public function indexAction()
-    {
-    }
+{  
+    protected $albumTable;
+
+     public function indexAction()
+     {
+         return new ViewModel(array(
+             'albums' => $this->getAlbumTable()->fetchAll(),
+         ));
+     }
     
     public function addAction()
     {
@@ -24,5 +29,14 @@ class AlbumController extends AbstractActionController
     public function deleteAction()
     {
         
+    }
+    
+    public function getAlbumTable()
+    {
+        if(!$this->albumTable){
+            $sm = $this->getServiceLocator();
+            $this->albumTable = $sm->get('Album\Model\AlbumTable');
+        }
+        return $this->albumTable;
     }
 }
